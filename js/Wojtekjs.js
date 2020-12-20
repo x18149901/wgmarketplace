d512d7c4338d992ac860a37c8573c10076c32aea
x18149901 <76390973+x18149901@users.noreply.github.com>
2020-12-20 11:38:35 +0000

    create wojtek js file

---
+++ js/Wojtekjs.js
@@ -0,0 +1,15 @@
+function contactfunction()
+{
+
+if(document.getElementById('contactmail').value==""){
+		alert("Please enter your e-mail");
+	}
+
+
+  	else{
+  		alert("Thank you for contacting us. We'll get back to you shortly.");
+  		}
+}
+
+
+
